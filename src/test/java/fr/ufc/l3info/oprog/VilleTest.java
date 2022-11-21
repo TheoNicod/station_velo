059ab036cdda9f20f03fd297c1bfbdf55a44aecb
Théo <theonicod@hotmail.Fr>
2022-11-21 18:39:41 +0100

    Ajout VilleTest + Exploitant

---
+++ src/test/java/fr/ufc/l3info/oprog/VilleTest.java
@@ -0,0 +1,167 @@
+package fr.ufc.l3info.oprog;
+
+import org.graalvm.compiler.hotspot.replacements.AssertionSnippets;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class VilleTest {
+
+    private Ville v;
+
+    @Before
+    public void beforeVille() {
+        this.v = new Ville();
+    }
+
+    /* Constructeur Ville() */
+
+    /* void initialiser(File f) throws IOException */
+    @Test
+    public void initialiserTestOk() {
+        v.initialiser(new File("./target/classes/data/stationsOK.txt"));
+    }
+
+    @Test(expected = IOException.class)
+    public void initialiserTestMauvaisChemin() throws IOException {
+        v.initialiser(new File("./target/classes/data/sta.txt"));
+    }
+
+    @Test (expected = IOException.class)
+    public void initialiserTestErreurFichier() {
+        v.initialiser(new File("./target/classes/data/stationsMultipleErreur.txt"));
+    }
+
+    // manque les test pour "On notera que chaque appel à cette méthode aura pour effet de réinitialiser l'ensemble des stations existantes. "
+
+    /* void setStationPrincipale(String st) */
+    @Test
+    public void setStationPrincipaleTest() {
+        v.setStationPrincipale("Avenue du Maréchal Foch");
+        ClosestStationIterator it = (ClosestStationIterator) v.iterator();
+        Assert.assertEquals("Avenue du Maréchal Foch", it.next().getNom());
+    }
+
+    /* Station getStation(String nom) */
+    @Test
+    public void getStationTestOk() {
+        v.initialiser(new File("./target/classes/data/stationsOK.txt"));
+        Station s = v.getStation("Avenue du Maréchal Foch");
+        Assert.assertNotEquals(null, s);
+        Assert.assertEquals("Avenue du Maréchal Foch", s.getNom());
+    }
+
+    @Test
+    public void getStationTestStationInexistante() {
+        v.initialiser(new File("./target/classes/data/stationsOK.txt"));
+        Station s = v.getStation("Avenue");
+        Assert.assertEquals(null, s);
+    }
+
+    /* Station getStationPlusProche(double lat, double lon) */
+    @Test
+    public void getStationPlusProcheTestPremiereStation() {
+        v.initialiser(new File("./target/classes/data/stationsOK.txt"));
+        Station s = v.getStationPlusProche(47.247761, 5.983599);
+        Assert.assertNotEquals(null, s);
+        Assert.assertEquals("21 - Avenue Fontaine Argent, Boulevard Diderot", s.getNom());
+    }
+
+    @Test
+    public void getStationPlusProcheTestDeuxiemeStation() {
+        v.initialiser(new File("./target/classes/data/stationsOK.txt"));
+        Station s = v.getStationPlusProche(47.24651, 6.02267);
+        Assert.assertNotEquals(null, s);
+        Assert.assertEquals("Avenue du Maréchal Foch", s.getNom());
+    }
+
+    /* Abonne creerAbonne(String nom, String RIB) */
+    @Test
+    public void creerAbonneTestOk() {
+        Abonne a = v.creerAbonne("Theo", "12345-55555-11111111111-47");
+        Assert.assertEquals("Theo", a.getNom());
+    }
+
+    @Test
+    public void creerAbonneTestNomVide() {
+        Assert.assertEquals(null, v.creerAbonne("", "12345-55555-11111111111-47"));
+    }
+
+    @Test
+    public void creerAbonneTestRibVide() {
+        Abonne a = v.creerAbonne("Theo", "");
+        Assert.assertTrue(a.estBloque());
+    }
+
+    /* Iterator<Station> iterator() */
+    @Test
+    public void iteratorTestStationPrincipale() { //bizarre
+        v.initialiser(new File("./target/classes/data/stationsOK.txt"));
+        ClosestStationIterator it = (ClosestStationIterator) v.iterator();
+        v.setStationPrincipale("Avenue du Maréchal Foch");
+        Assert.assertEquals("Avenue du Maréchal Foch", it.next());
+    }
+
+    @Test
+    public void iteratorTestNombre() { //bizarre
+        v.initialiser(new File("./target/classes/data/stationsOK.txt"));
+        ClosestStationIterator it = (ClosestStationIterator) v.iterator();
+        int i = 0;
+        while(it.hasNext()) {
+            it.next();
+            i++;
+        }
+        Assert.assertEquals(2, i);
+    }
+
+    /* Map<Abonne, Double> facturation(int mois, int annee)  */
+    @Test
+    public void facturationTestDateInvalide() {
+        Assert.assertEquals(null, v.facturation(0, 2022));
+        Assert.assertEquals(null, v.facturation(13, 2022));
+        Assert.assertEquals(null, v.facturation(1, 2023));
+        Assert.assertEquals(null, v.facturation(1, -1));
+    }
+
+    @Test
+    public void facturationTest() throws IncorrectNameException{
+        Abonne a = new Abonne("Theo", "12345-55555-11111111111-47");
+        Abonne b = new Abonne("Romain", "12345-55555-22222222222-47");
+        Abonne c = new Abonne("Hugo", "12345-55555-33333333333-47");
+
+        IVelo ve [] = new Velo [5];
+        FabriqueVelo fab = FabriqueVelo.getInstance();
+        for(int i = 0; i < 5; i++) {
+            ve[i] = fab.construire('h', "CADRE_ALUMINIUM");
+        }
+
+        IRegistre r = new JRegistre();
+        r.emprunter(a, ve[0], System.currentTimeMillis());
+        r.emprunter(b, ve[1], System.currentTimeMillis());
+        r.emprunter(c, ve[2], System.currentTimeMillis());
+        r.emprunter(a, ve[3], System.currentTimeMillis());
+        r.emprunter(b, ve[4], System.currentTimeMillis());
+
+        for(int i = 0; i < 5; i++) r.retourner(ve[i], System.currentTimeMillis() + 3600000);
+        // == emprunt velo cadre alu 1h (x2 pour Théo)
+
+        Map<Abonne, Double> facturation_mois;
+        facturation_mois = v.facturation(11, 2022);
+
+        Assert.assertEquals(3, facturation_mois.size());
+        Assert.assertEquals(4.4, facturation_mois.get("Theo"), 0.00);
+        Assert.assertEquals(2.2, facturation_mois.get("Romain"), 0.00);
+        Assert.assertEquals(2.2, facturation_mois.get("Hugo"), 0.00);
+    }
+
+
+
+
+
+
+
+}
