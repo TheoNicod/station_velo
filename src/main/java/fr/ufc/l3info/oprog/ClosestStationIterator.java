6aa099ce2eee83df99420dc3e0759b330c64188f
romainChuat <romain.chuat@outlook.com>
2022-11-22 17:04:55 +0100

    Ville v1

---
+++ src/main/java/fr/ufc/l3info/oprog/ClosestStationIterator.java
@@ -0,0 +1,25 @@
+package fr.ufc.l3info.oprog;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+
+public class ClosestStationIterator implements Iterator<Station>{
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Station next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+    }
+
+
+}
--- src/main/java/fr/ufc/l3info/oprog/Ville.java
+++ src/main/java/fr/ufc/l3info/oprog/Ville.java
@@ -1,13 +1,86 @@
 package fr.ufc.l3info.oprog;
 
+
+import fr.ufc.l3info.oprog.parser.*;
+
+import javax.swing.text.html.parser.Parser;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+public class Ville implements Iterable<Station>{
 
-public class Ville {
-    Ville(){}
+    Exploitant exploitant;
+    Station stationPrincipal;
+    Set<Station> stationSet;
+    final StationParser parser = StationParser.getInstance();
 
-    void initialiser(File f) {}
+    Ville(){
+        exploitant = new Exploitant();
+        stationSet = new HashSet<Station>();
+    }
+    void initialiser(File f) throws IOException {
+        ASTNode n = null;
+        try{
+             n = parser.parse(f);
+        }catch (StationParserException e){
+            System.out.println(e.getMessage());
+        }
+        ASTCheckerVisitor visitor = new ASTCheckerVisitor();
+        n.accept(visitor);
+        ASTStationBuilder builder  =  new ASTStationBuilder();
+        if(visitor.getErrors().size() == 0 ){
+            for(Station s : builder.getStations()){
+                stationSet.add(s);
+            }
+        }
+        Iterator<Station> iter = iterator();
+        stationPrincipal = iter.next();
+    }
+    void setStationPrincipale(String st){
+        for(Station s: stationSet) {
+            if (s.getNom() == st) {
+                stationPrincipal = s;
+            }
+        }
+    }
+    public Station getStation(String nom){
+        for(Station s: stationSet){
+            if(s.getNom() == nom){
+                return s;
+            }
+        }
+        return  null;
+    }
+    public Station getStationPlusProche(double lat, double lon){
+        Station temp = new Station("temp",lat,lon,0);
+        double shortestDistance = stationPrincipal.distance(temp);
+        Station closest = null;
+        for(Station s: stationSet){
+            double currentDistance = s.distance(temp);
+            if(currentDistance < shortestDistance){
+                shortestDistance = currentDistance;
+                closest = s;
+            }
+        }
+        return closest;
+    }
+    public Abonne creerAbonne(String nom, String RIB){
+        Abonne abonne = null;
+        try {
+            abonne = new Abonne(nom, RIB);
+        }catch (IncorrectNameException e){
+            System.out.println(e.getMessage());
+        }
+        return abonne;
+    }
+
+    public Iterator<Station> iterator(){
+        return new ClosestStationIterator();
+    }
 
 
-
-
+    Map<Abonne, Double> facturation(int mois, int annee){return null;}
 }
