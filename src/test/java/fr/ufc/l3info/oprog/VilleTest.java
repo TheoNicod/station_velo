b253a19fa5c9e6e2b41d14d00c9887b2dc5d746f
HugoRGT <rougetethugo@gmail.com>
2022-11-25 15:27:37 +0100

    Facturation method

---
+++ src/test/java/fr/ufc/l3info/oprog/VilleTest.java
@@ -0,0 +1,12 @@
+package fr.ufc.l3info.oprog;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VilleTest {
+    Ville v;
+    @Before
+    public void BeforeVelo(){
+        this.v = new Ville();
+    }
+}
